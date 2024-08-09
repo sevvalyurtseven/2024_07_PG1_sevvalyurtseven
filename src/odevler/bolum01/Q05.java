819cfe92f7a02b016392246622ee5f58398f3189
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-08-09 19:35:02 +0300

    compute expressions

---
+++ src/odevler/bolum01/Q05.java
@@ -0,0 +1,10 @@
+package odevler.bolum01;
+
+public class Q05 {
+    public static void main(String[] args) {
+        double value;
+        value = (9.5 * 4.5 - 2.5 * 3) / 45.5 - 3.5;
+        System.out.println(value);
+    }
+
+}
