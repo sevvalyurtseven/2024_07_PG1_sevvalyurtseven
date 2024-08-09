503ff1f936677f1dbb62ea3862d8380bfa1690b4
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-08-09 20:30:22 +0300

    area and perimeter of a rectangle

---
+++ src/odevler/bolum01/Q09.java
@@ -0,0 +1,10 @@
+package odevler.bolum01;
+
+public class Q09 {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height = 7.9;
+        double area = width * height;
+        System.out.println(area);
+    }
+}
