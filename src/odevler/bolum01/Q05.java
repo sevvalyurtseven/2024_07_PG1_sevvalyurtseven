fcffa3a5dc3ec8e69058b0d199d4609f0f6461f5
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-08-12 02:58:30 +0300

    comment

--- src/odevler/bolum01/Q05.java
+++ src/odevler/bolum01/Q05.java
@@ -1,7 +1,7 @@
 package odevler.bolum01;
 
 public class Q05 {
-    // 1.5 (Compute expressions) Write a program that displays the result of
+    // 1.5 (Compute expressions) Write a program that displays the result of (9.5 × 4.5 - 2.5 * 3) / 45.5 - 3.5
     public static void main(String[] args) {
         double value; // variable declaration
         value = (9.5 * 4.5 - 2.5 * 3) / 45.5 - 3.5;
