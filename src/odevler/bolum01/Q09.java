cdae93b85febc57557089f35ed1fabc2f3abfda5
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-08-12 03:23:06 +0300

    area of the rectangle:

--- src/odevler/bolum01/Q09.java
+++ src/odevler/bolum01/Q09.java
@@ -1,10 +1,18 @@
 package odevler.bolum01;
 
 public class Q09 {
+    // 1.9 (Area and perimeter of a rectangle) Write a program that displays the area and perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following formula:
+                   /* area = width * height */
     public static void main(String[] args) {
+        // Define the width
         double width = 4.5;
+
+        // Define the height
         double height = 7.9;
+
+        // Calculate the area
         double area = width * height;
-        System.out.println(area);
+
+        System.out.println("Area of the rectangle: " + area);
     }
 }
